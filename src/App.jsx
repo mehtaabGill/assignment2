165ce90346004c36c1eb4b99a3b7cf38c335d73e
Mehtaab Gill <hello@mehtaab.dev>
2025-04-07 19:37:47 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className="">
       <Routes>
-        <Route path='/' Component={Login} />
         <Route path='/login' Component={Login} />
         <Route path='/galleries' Component={Galleries} />
         <Route path='/artists' Component={Artists} />
